236140b0c85f6719616d5a777c269ab35f8d2905
zhiruisong02 <zjs5301@psu.edu>
2020-09-17 15:33:34 +0000

    delete n in line 14

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# Author: Song Zhirui zjs5301@psu.edu
+# Collaborator: Jack Hillman jsh5719@psu.edu
+# Collaborator:  Shrey Hillman sxs6588@psu.edu
+# Collaborator: Alexandros Condeelis afc5865@psu.edu
+
 def sum_n(n):
   if n==0:
     return n
@@ -6,10 +11,10 @@ def sum_n(n):
 
 def print_n(s,n):
   if n==0:
-    return n
+    return
   else:
-   print(s) 
-   print_n(s,n-1)
+   print(s)
+   print_n(s,n-1) 
 def run():
   n = input ("Enter an int: ")
   n = int(n)
